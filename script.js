c74002d07e33a503fc67967ef6af464e7f264072
Jasoooon02 <kelemenjanos39@gmail.com>
2024-10-07 11:02:10 +0200

    .

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add event listeners to the navigation links
-document.querySelectorAll('header nav a').forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href').slice(1);
-        document.querySelector(`#${targetId}`).scrollIntoView({ behavior: 'smooth' });
-    });
-});
