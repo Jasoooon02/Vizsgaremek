625c75ccd730d35f34f25711b266852dc7e5ba2b
Jasoooon02 <kelemenjanos39@gmail.com>
2024-10-07 13:52:34 +0200

    .

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = "root"; 
+$dbname = "user_db"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Kapcsolat hiba: " . $conn->connect_error . "" );
+}
+$sql="INSERT INTO users (id,username,password) VALUES ()";
+echo "Sikeresen kapcsolódott az adatbázishoz.";
+?>
