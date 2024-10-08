8821b141f420062be566b0832a6a88e631ff4bcf
Jasoooon02 <kelemenjanos39@gmail.com>
2024-10-08 13:43:42 +0200

    .

--- login.php
+++ login.php
@@ -9,6 +9,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kapcsolat hiba: " . $conn->connect_error . "" );
 }
-$sql="INSERT INTO users (id,username,password) VALUES ()";
+//$sql="INSERT INTO users (id,username,password) VALUES ()";
 echo "Sikeresen kapcsolódott az adatbázishoz.";
 ?>
