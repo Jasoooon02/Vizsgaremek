d590ca74a98e34bfdab915b19f19ec5cee9e7e87
Jasoooon02 <kelemenjanos39@gmail.com>
2024-12-09 11:35:26 +0100

    .

--- get_price.php
+++ get_price.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'carss');
+$conn = new mysqli('localhost', 'root', '', 'cars_db');
 $conn->set_charset("utf8");
 if (isset($_GET['name']) && isset($_GET['engine'])) {
     $name = $conn->real_escape_string($_GET['name']);
