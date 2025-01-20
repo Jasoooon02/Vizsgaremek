67fc487edc0f04b34c3ea188dd4fab7dde30fe0d
Jasoooon02 <kelemenjanos39@gmail.com>
2025-01-20 13:03:04 +0100

    .

--- verify_otp.php
+++ verify_otp.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OTP Ellenőrzés</title>
-    <link rel="stylesheet" href="forgott.css">
+    <link rel="stylesheet" href="css/forgott.css">
 </head>
 <body>
 <?php
