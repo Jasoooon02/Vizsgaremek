b66d1f411823e43e6cb96c11e365229ea47d48f0
Jasoooon02 <kelemenjanos39@gmail.com>
2025-01-23 11:07:23 +0100

    .

--- logout.js
+++ logout.js
@@ -1,47 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     const userIcon = document.getElementById("user-icon");
 
-
+ 
     function isLoggedIn() {
-        return localStorage.getItem("isLoggedIn") === "true";
-    }
-
-    function updateIcon() {
-        if (isLoggedIn()) {
-            userIcon.title = "Kijelentkezés";
-        } else {
-            userIcon.title = "Bejelentkezés";
-        }
+        return localStorage.getItem("loggedIn") === "true";
     }
 
     
+
+   
     updateIcon();
 
-    
+
     userIcon.addEventListener("click", () => {
         if (isLoggedIn()) {
             
             const confirmLogout = confirm("Biztosan ki szeretnél jelentkezni?");
             if (confirmLogout) {
-                
-                fetch('logout.php', { method: 'POST' })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.success) {
-                            
-                            localStorage.removeItem("isLoggedIn");
-                            localStorage.removeItem("username");
-                            alert(data.message);
-                            
-                            window.location.href = "index.html";
-                        } else {
-                            alert("Hiba történt a kijelentkezés során!");
-                        }
-                    })
-                    .catch(error => {
-                        console.error("Hiba a kijelentkezés során:", error);
-                        alert("Nem sikerült kapcsolatot létesíteni a szerverrel.");
-                    });
+                localStorage.removeItem("loggedIn");
+                alert("Sikeresen kijelentkeztél!");
+                updateIcon(); 
+                location.reload();  
             }
         } else {
             
